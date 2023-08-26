52d7bd4d102fb96fbb9d735b74c498b7a6417f85
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-08-26 23:27:04 +0530

    Create deletion_atend.c

---
+++ Linked_List/Delition/deletion_atend.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+  int data;
+  struct Node* next;
+};
+
+void triversal(struct Node* head){
+  struct Node* ptr = head;
+  while (ptr->next != NULL){
+    printf("%d\n", ptr->data);
+    ptr = ptr -> next;
+  }
+}
+
+struct Node* delition_atend(struct Node* head){
+  struct Node* ptr = head;
+  while (ptr -> next != NULL){
+    ptr = ptr -> next;
+  }
+  free(ptr);
+  return head;
+}
+
+int main(){
+  struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* first = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* fourth = (struct Node*)malloc(sizeof(struct Node));
+  struct Node* fifth = (struct Node*)malloc(sizeof(struct Node));
+  head -> data = 0;
+  head -> next = first;
+  first -> data = 1;
+  first -> next = second;
+  second -> data = 2;
+  second -> next = third;
+  third -> data = 3;
+  third -> next = fourth;
+  fourth -> data = 4;
+  fourth -> next = fifth;
+  fifth -> data = 5;
+  fifth -> next = NULL;
+  
+  printf("before delition\n");
+  triversal(head);
+  head = delition_atend(head);
+  printf("after delition\n");
+  triversal(head);
+  return 0;
+}
